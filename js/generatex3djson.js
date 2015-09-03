ebaff62e1e6eecf4f294681e8493f16403d7e8a3
John Carlson <johncarlson@Johns-MacBook-Air.local>
2015-09-03 17:38:33 -0500

    First kind of working case

---
+++ js/generatex3djson.js
@@ -0,0 +1,8 @@
+var context = new Jsonix.Context([x3djson]);
+
+var unmarshaller = context.createUnmarshaller();
+
+unmarshaller.unmarshalURL('HelloWorld.x3d',
+    function (unmarshalled) {
+        document.write(JSON.stringify(unmarshalled, null, 2));
+    });
---
+++ js/validatex3djson.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var Ajv = require('ajv');
+
+var XMLSchemaJsonSchema = JSON.parse(fs.readFileSync('./XMLSchema.jsonschema').toString());
+var JsonixJsonSchema = JSON.parse(fs.readFileSync('./Jsonix.jsonschema').toString());
+var X3dJsonSchema = JSON.parse(fs.readFileSync('./mappings/x3djson.jsonschema').toString());
+
+var ajv = new Ajv();
+ajv.addSchema(XMLSchemaJsonSchema, 'http://www.jsonix.org/jsonschemas/w3c/2001/XMLSchema.jsonschema');
+ajv.addSchema(JsonixJsonSchema, 'http://www.jsonix.org/jsonschemas/jsonix/Jsonix.jsonschema');
+var validate = ajv.compile(X3dJsonSchema);
+
+var hello = JSON.parse(fs.readFileSync("../../www.web3d.org/x3d/content/examples/X3dForWebAuthors/HelloWorld.json").toString());
+
+var valid = validate(hello);
+if (!valid) {
+    console.log('Validation failed.');
+    console.log('Validation errors:');
+    console.log(validate.errors);
+}
+
