9228c2d3140f67b0f420cc2e43c34b4a631e2830
John Carlson <johncarlson@Johns-MacBook-Air.local>
2015-09-04 04:08:41 -0500

    Tried to convert from web to node.js

--- js/generatex3djson.js
+++ js/generatex3djson.js
@@ -1,3 +1,6 @@
+// var Jsonix = require('jsonix').Jsonix;
+// var x3djsonSchema = require('../mappings/x3djson.jsonschema').x3djson;
+
 var context = new Jsonix.Context([x3djson]);
 
 var unmarshaller = context.createUnmarshaller();
