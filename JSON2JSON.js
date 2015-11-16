aba0992036042e6f0015950252bf779b3140a9a0
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-11-16 06:11:19 -0600

    added comments

--- JSON2JSON.js
+++ JSON2JSON.js
@@ -1,12 +1,14 @@
-// Convert JSONIX XML to JSON output format to X3D JSON
+// Convert JSONIX XML to JSON output format to X3D JSON (on standard input)
+
+// see js/StyleSheet.js for actual code
+
+var stylesheet = require('./js/StyleSheet');
 
 var content = [];
 // read content into buffer
 process.stdin.on('data', function(buf) {
 	content.push(buf);
 });
-
-var stylesheet = require('./js/StyleSheet');
 
 process.stdin.on('end', function() {
 	var prototypes = JSON.parse(content.join());
