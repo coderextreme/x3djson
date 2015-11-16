cfc43d7253a3c05bba9be2fbc335247e98d43594
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-11-16 06:09:41 -0600

    Changed reading to read into string array instead of long string.

--- JSON2JSON.js
+++ JSON2JSON.js
@@ -1,15 +1,15 @@
 // Convert JSONIX XML to JSON output format to X3D JSON
 
-var content = '';
+var content = [];
 // read content into buffer
-process.stdin.resume();
-process.stdin.on('data', function(buf) { content += buf.toString(); });
+process.stdin.on('data', function(buf) {
+	content.push(buf);
+});
 
 var stylesheet = require('./js/StyleSheet');
 
 process.stdin.on('end', function() {
-	var prototypes = JSON.parse(content);
-	var object = {};
+	var prototypes = JSON.parse(content.join());
 	var json = stylesheet(prototypes);
 	console.log(JSON.stringify(json, null, 2));
 });
